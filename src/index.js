da43afaec921e7e21b16995f0862f386fb44181e
Balram Pandey <balrampandey19@gmail.com>
2022-05-31 11:16:34 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -11,4 +11,26 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight); 
 document.body.appendChild(renderer.domElement);
+
+// Crrating a cube
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+const ambient_light = new THREE.AmbientLight(0x404040);
+const directional_light = new THREE.DirectionalLight(0xffffff,1);
+ambient_light.add(directional_light);
+scene.add(ambient_light)
+
+function animate() {
+    cube.rotation.x += 0.05
+    cube.rotation.y += 0.05
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
+
 renderer.render( scene, camera );
