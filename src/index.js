4ee9c7c38888af36a1dcce540ddc8c7bb7253cbd
Balram Pandey <balrampandey19@gmail.com>
2022-06-01 09:03:25 +0530

    geometry learn

--- src/index.js
+++ src/index.js
@@ -19,6 +19,30 @@ const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+
+
+// Create a cone
+
+
+const cone_geometry = new THREE.ConeGeometry( 5, 20, 32 );
+const cone_material = new THREE.MeshPhongMaterial( {color: 0xed810a} );
+const cone = new THREE.Mesh( cone_geometry, cone_material );
+cone.position.set(-10,5,0);
+scene.add( cone );
+
+
+// Create cylinder Geometry
+
+
+const geometry_cyl = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+const material_cyl = new THREE.MeshPhongMaterial( {color: 0xbaa019} );
+const cylinder = new THREE.Mesh( geometry_cyl, material_cyl );
+cylinder.position.set(20,5,0);
+
+scene.add( cylinder );
+
+
+
 camera.position.z = 5;
 
 // set up a flat space of the Meta verse
@@ -39,14 +63,24 @@ ambient_light.add(directional_light);
 scene.add(ambient_light)
 
 
+
+
+
 camera.position.set(10,5,40);
 
 function animate() {
     cube.rotation.x += 0.05
     cube.rotation.y += 0.05
+    cone.rotation.x += 0.01
+    cone.rotation.y += 0.01
+
+    cylinder.rotation.x += 0.01
+    cylinder.rotation.y += 0.01
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
 animate();
+
+
 
 renderer.render( scene, camera );
