5bf9e00caf39d61f0ae173e0b5f2114de6495567
Balram Pandey <balrampandey19@gmail.com>
2022-06-01 08:45:49 +0530

    geometry space

--- src/index.js
+++ src/index.js
@@ -20,10 +20,26 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
-const ambient_light = new THREE.AmbientLight(0x404040);
+
+// set up a flat space of the Meta verse
+
+const geometry_space = new THREE.BoxGeometry(100,0.2,50);
+const material_space = new THREE.MeshPhongMaterial({color:0xffffff});
+const space = new THREE.Mesh(geometry_space,material_space);
+scene.add(space);
+
+
+
+
+// Setting the scene light
+
+const ambient_light = new THREE.AmbientLight(0xbda355);
 const directional_light = new THREE.DirectionalLight(0xffffff,1);
 ambient_light.add(directional_light);
 scene.add(ambient_light)
+
+
+camera.position.set(10,5,40);
 
 function animate() {
     cube.rotation.x += 0.05
